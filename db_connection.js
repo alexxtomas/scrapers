85ab52942349c80ad03c568fdd0995fd4b907363
alexxtomas <atomasllimos@gmail.com>
2023-01-15 19:33:49 +0100

    Refactor

--- db_connection.js
+++ db_connection.js
@@ -2,14 +2,15 @@ import mongoose from 'mongoose'
 
 // Pega tu URI de conexión a MongoDB aquí
 const URI =
-  'mongodb+srv://root:root@cluster0.fvg4s0k.mongodb.net/star-wars?retryWrites=true&w=majority'
+  'mongodb+srv://root:root@cluster0.fvg4s0k.mongodb.net/Amazon?retryWrites=true&w=majority'
 
 // Nos conectamos a la base de datos
-export async function connectToDb() {
-  try {
-    await mongoose.connect(URI, { useNewUrlParser: true }).set('strictQuery', false)
-    console.log('Connected to the DB 👀')
-  } catch (error) {
-    console.error('Failed to connect to the DB 💔', error)
-  }
-}
+console.log('Connecting to database...🤖')
+await mongoose
+  .set('strictQuery', false)
+  .connect(URI)
+  .then((db) => console.log(`Connected to database -> ${db.connection.name} ✅`))
+  .catch((err) => {
+    console.error('Failed to connect to database ❌')
+    throw err
+  })
