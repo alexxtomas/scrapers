4ea028c5658c12ec9ce88786446d61f116f3f441
alexxtomas <atomasllimos@gmail.com>
2023-01-15 19:34:31 +0100

    Style product model

--- product_model.js
+++ product_model.js
@@ -1,16 +1,22 @@
 import { model, Schema } from 'mongoose'
 
-const productSchema = new Schema({
-  title: { type: String, required: true, unique: true },
-  image: {
-    type: String,
-    required: true,
-    default: 'https://www.odoo.com/web/image/res.users/1072846/image_1024?unique=3f33558'
+const productSchema = new Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    image: {
+      type: String,
+      required: true,
+      default: 'https://www.odoo.com/web/image/res.users/1072846/image_1024?unique=3f33558'
+    },
+    link: { type: String, required: true, unique: true },
+    price: { type: String, required: true },
+    reviews: Number,
+    stars: String
   },
-  link: { type: String, required: true, unique: true },
-  price: { type: String, required: true },
-  reviews: { type: Number },
-  stars: { type: String }
-})
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
 
 export const Product = model('Product', productSchema)
