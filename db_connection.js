0bb9bd4c7fc5572456aa8dbb7697031a18631e29
alexxtomas <atomasllimos@gmail.com>
2023-01-15 20:21:37 +0100

    Remove URI from function

--- db_connection.js
+++ db_connection.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose'
 
 // Pega tu URI de conexión a MongoDB aquí
-const URI =
-  'mongodb+srv://root:root@cluster0.fvg4s0k.mongodb.net/Amazon?retryWrites=true&w=majority'
 
 // Nos conectamos a la base de datos
-console.log('Connecting to database...🤖')
-await mongoose
-  .set('strictQuery', false)
-  .connect(URI)
-  .then((db) => console.log(`Connected to database -> ${db.connection.name} ✅`))
-  .catch((err) => {
-    console.error('Failed to connect to database ❌')
-    throw err
-  })
+export async function connectToDb(URI) {
+  console.log('Connecting to database...🤖')
+  await mongoose
+    .set('strictQuery', false)
+    .connect(URI)
+    .then((db) => console.log(`Connected to database -> ${db.connection.name} ✅`))
+    .catch((err) => {
+      console.error('Failed to connect to database ❌')
+      throw err
+    })
+}
