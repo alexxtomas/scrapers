19ceb7d8f7769015c1a000a9d6515a2388643254
alexxtomas <atomasllimos@gmail.com>
2023-01-15 19:34:13 +0100

    Add function to connect to database

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { load } from 'cheerio'
+import './db_connection.js'
+import { Product } from './product_model.js'
 
 const URL =
   'https://www.amazon.es/s?k=straps&crid=2M8FD9OZE91ZJ&sprefix=%2Caps%2C92&ref=nb_sb_ss_recent_1_0_recent'
 
 async function fetchProduct() {
-  console.log('Fetching Products...🤑')
+  console.log('Fetching Products...🤖')
   try {
     const response = await fetch(URL)
     const html = await response.text()
@@ -56,6 +58,14 @@ async function fetchProduct() {
   }
 }
 
-fetchProduct().then((products) => {
+fetchProduct().then(async (products) => {
   console.log(`Fetched ${products.length} products successfully ✅`)
+  console.log('Saving to database...🤖')
+
+  await Product.insertMany(products)
+    .then(() => console.log('Products saved succesfully to database ✅'))
+    .catch((err) => {
+      console.error('Something went wrong when saving products to database ❌')
+      throw err
+    })
 })
