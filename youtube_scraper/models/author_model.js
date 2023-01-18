e0169527aa5fc946a8d4d02a52384f2f961645ec
alexxtomas <atomasllimos@gmail.com>
2023-01-18 16:16:42 +0100

    Create author model

---
+++ youtube_scraper/models/author_model.js
@@ -0,0 +1,18 @@
+import { model, Schema } from 'mongoose'
+
+const authorSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    user: { type: String, required: true },
+    channelURL: { type: String, required: true },
+    userURL: { type: String, required: true },
+    verified: { type: Boolean, required: true },
+    suscribers: { type: Number, required: true }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+export const Author = model('Author', authorSchema)
