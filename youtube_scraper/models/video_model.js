2a2876a14a6687aae470f3e0d5d66373a426e7c6
alexxtomas <atomasllimos@gmail.com>
2023-01-18 16:14:22 +0100

    Fix create video_model

--- youtube_scraper/models/video_model.js
+++ youtube_scraper/models/video_model.js
@@ -1,19 +1,27 @@
 import { model, Schema } from 'mongoose'
 
-const videoSchema = new Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true, default: '' },
-  videoDuration: { type: String, required: true },
-  isFamilySafe: { type: Boolean, required: true },
-  viewCount: { type: Number, required: true },
-  category: { type: String, required: true },
-  publishDate: { type: String, required: true },
-  keywords: { type: [String], required: true },
-  author: { type: Schema.Types.ObjectId, required: true },
-  isPrivate: { type: Boolean, required: true },
-  isLoveContent: { type: Boolean, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  ageRestricted: { type: Boolean, required: false },
-  videoURL: { type: String, required: true }
-})
+const videoSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true, default: '' },
+    videoDuration: { type: String, required: true },
+    isFamilySafe: { type: Boolean, required: true },
+    viewCount: { type: Number, required: true },
+    category: { type: String, required: true },
+    publishDate: { type: String, required: true },
+    keywords: { type: [String], required: true },
+    author: { type: Schema.Types.ObjectId, required: true },
+    isPrivate: { type: Boolean, required: true },
+    isLoveContent: { type: Boolean, required: true },
+    likes: { type: Number, required: true },
+    dislikes: { type: Number, required: true },
+    ageRestricted: { type: Boolean, required: false },
+    videoURL: { type: String, required: true }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
+
+export const Video = model('Video', videoSchema)
