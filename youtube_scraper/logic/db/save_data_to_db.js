0f998ef0356135d4df082df9505b480395fad857
alexxtomas <atomasllimos@gmail.com>
2023-01-19 11:48:16 +0100

    Create function to save data to db

---
+++ youtube_scraper/logic/db/save_data_to_db.js
@@ -0,0 +1,15 @@
+import { Author } from '../../models/author_model.js'
+import { Video } from '../../models/video_model.js'
+import { log } from '../log'
+export async function saveDataToDb({ authors, videos }) {
+  Promise.all([await Author.insertMany(authors), await Video.insertMany(videos)])
+    .then(() => {
+      log({
+        message: 'All videos and their authors have been saved to the database successfully 🚀'
+      })
+    })
+    .catch((err) => {
+      log({ message: 'Error while saving data to database ❌', err: true })
+      throw err
+    })
+}
