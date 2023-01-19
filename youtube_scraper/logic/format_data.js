9f3b52ea6482f84d3fbbe520b6e0e1840bdbe2bc
alexxtomas <atomasllimos@gmail.com>
2023-01-19 12:13:08 +0100

    Move logic to format data to format_data file

---
+++ youtube_scraper/logic/format_data.js
@@ -0,0 +1,61 @@
+import ytdl from 'ytdl-core'
+export async function formatData({ authors, videos }) {
+  for (const i in videos) {
+    const video = videos[i]
+    const { link } = video
+    try {
+      const { videoDetails } = await ytdl.getInfo(link)
+
+      const {
+        name,
+        user,
+        channel_url: channelURL,
+        user_url: userURL,
+        verified,
+        subscriber_count: subscribers
+      } = videoDetails.author
+      authors[i] = {
+        name,
+        user,
+        channelURL,
+        userURL,
+        verified,
+        subscribers: subscribers.toString()
+      }
+      const {
+        description,
+        lengthSeconds: videoDuration,
+        isFamilySafe,
+        viewCount,
+        category,
+        publishDate,
+        keywords,
+        isPrivate,
+        isLiveContent,
+        likes,
+        dislikes,
+        age_restricted: ageRestricted,
+        video_url: videoURL
+      } = videoDetails
+      videos[i] = {
+        // author: authors[i],
+        ...video,
+        description: description ?? '',
+        videoDuration: `${videoDuration} seconds`,
+        isFamilySafe,
+        viewCount,
+        category,
+        publishDate,
+        keywords: keywords ?? [],
+        isPrivate,
+        isLiveContent,
+        likes: likes ?? 'Unknown',
+        dislikes: dislikes ?? 'Unknown',
+        ageRestricted,
+        videoURL
+      }
+    } catch (err) {
+      console.error(`Error when formatting ${video.title} ❌`)
+    }
+  }
+}
