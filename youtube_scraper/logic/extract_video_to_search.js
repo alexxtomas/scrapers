7fa68b10c1792fcfeef4710a1b92122b3ac297b2
alexxtomas <atomasllimos@gmail.com>
2023-01-19 12:21:56 +0100

    Extract first index of valueProvided

--- youtube_scraper/logic/extract_video_to_search.js
+++ youtube_scraper/logic/extract_video_to_search.js
@@ -1,17 +1,20 @@
 export function extractVideoToSearch() {
-  const [, , ...videoToSearch] = process.argv
+  const [, , ...valueProvided] = process.argv
 
-  if (videoToSearch.length > 1) {
+  if (valueProvided.length > 1) {
     console.log(
       'If the product name contains spaces, enter the product name in quotes like this "star wars"🤖'
     )
     process.exit(1)
   }
 
-  if (!videoToSearch.length) {
+  if (!valueProvided.length) {
     console.log('Please provide a product to search!!!🤖')
     process.exit(1)
   }
+  const [videoToSearch] = valueProvided
 
   return { videoToSearch, formattedVideoToSearch: videoToSearch.replaceAll(' ', '_') }
 }
+
+extractVideoToSearch()
